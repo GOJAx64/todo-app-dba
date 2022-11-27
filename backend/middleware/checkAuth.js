b84a15e3d9fa4c68d1628189615ae5fbe609d441
GOJABoop <josealbertogarcia453@gmail.com>
2022-11-27 10:43:16 -0600

    Separating backend and frontend

---
+++ backend/middleware/checkAuth.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
+
+const checkAuth = async(req, res, next) => {
+    let token;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            token = req.headers.authorization.split(' ')[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = await User.findById(decoded.id).select("-password -confirmed -token -createdAt -updatedAt -__v");
+            return next();
+        } catch (error) {
+            return res.status(404).json({ msg: 'Hubo un error' });
+        }
+    }
+
+    if(!token) {
+        const error = new Error("Invalid Token");
+        return res.status(401).json({ msg: error.message });
+    }
+    next();
+}
+
+export default checkAuth;
